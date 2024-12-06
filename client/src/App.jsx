7d9c61c744be7496e7187235a8ab98e2d0dd57f7
fazzy12 <kaluifeanyi00@gmail.com>
2024-12-06 12:13:15 +0100

    create and mount pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home />
+      <BuyCredit />
+      <Result />
+    </div>
   )
 }
 
