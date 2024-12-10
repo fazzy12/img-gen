0b55c45e557ecc1e69024f18fb748fe1d924076a
fazzy12 <kaluifeanyi00@gmail.com>
2024-12-10 04:15:33 +0100

    add hero swction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import BuyCredit from "./pages/BuyCredit";
 import Result from "./pages/Result";
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
     min-h-screen bg-gradient-to-b from-teal-50 to-orange-50"
     >
       <NavBar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/buy" element={<BuyCredit />} />
