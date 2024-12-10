628944ffc5e0db367ebbaf22d5bd86fbca6db9b6
fazzy12 <kaluifeanyi00@gmail.com>
2024-12-10 13:28:25 +0100

    add Description section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import Steps from '../components/Steps'
-import Header from '../components/Header'
+import React from "react";
+import Steps from "../components/Steps";
+import Header from "../components/Header";
+import Description from "../components/Description";
 
 function Home() {
   return (
     <div>
-       <Header />
-       <Steps />
+      <Header />
+      <Steps />
+      <Description />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
