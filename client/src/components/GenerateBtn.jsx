2864053326e80c049258c3063b0fe7350c236782
fazzy12 <kaluifeanyi00@gmail.com>
2024-12-16 20:11:00 +0100

    add generate setion animation

--- client/src/components/GenerateBtn.jsx
+++ client/src/components/GenerateBtn.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { motion } from "motion/react";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 function GenerateBtn() {
+  const { user, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    if (user) {
+      navigate("/result");
+    } else {
+      setShowLogin(true);
+    }
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0.2, y: 100 }}
@@ -15,7 +28,10 @@ function GenerateBtn() {
       <h1 className="text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold text-neutral-800 py-6">
         See the magic, Try now
       </h1>
-      <button className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500">
+      <button
+        onClick={onClickHandler}
+        className="inline-flex items-center gap-2 px-12 py-3 rounded-full bg-black text-white m-auto hover:scale-105 transition-all duration-500"
+      >
         Generate Images <img src={assets.star_group} alt="" className="h-6" />
       </button>
     </motion.div>
