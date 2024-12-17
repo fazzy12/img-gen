eee90828f2b47acf87fd53cc7f9052ee771d0e61
fazzy12 <kaluifeanyi00@gmail.com>
2024-12-17 10:36:12 +0100

    add login in user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,27 +4,50 @@ import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
   try {
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-        return res.json({sucess: false, message: "missing details"});
+      return res.json({ sucess: false, message: "missing details" });
     }
-    const salt = await bcrypt.genSalt(10)
-    const hasedPassword = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const hasedPassword = await bcrypt.hash(password, salt);
 
     const userData = {
-        name,
-        email,
-        password: hasedPassword
+      name,
+      email,
+      password: hasedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ sucess: true, token, user: { name: user.name } });
+  } catch (e) {
+    console.log(e);
+    res.json({ sucess: false, message: e.message });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: true, message: "user does not exist" });
     }
 
-    const newUser = new userModel(userData)
-    const user = await newUser.save()
-    const token =  jwt.sign({id: user._id}, process.env.JWT_SECRET)
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    res.json({sucess: true, token,  user: {name: user.name}})
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ sucess: true, token, user: { name: user.name } });
+    } else {
+      return res.json({ success: true, message: "incorrect credentials" });
+    }
   } catch (e) {
-    console.log(e)
-    res.json({sucess: false, message: e.message})
+    console.log(e);
+    res.json({ success: false, message: e.message });
   }
 };
